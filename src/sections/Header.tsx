2be5274e0f9a5fbff39e4c89c2cec4e6b5954f2a
Koussay Hajri <hajrikoussay03@gmail.com>
2024-12-24 22:42:20 +0100

    Add AboutSection and TechIcon components; enhance Header and Testimonials styling

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <div className="w-full flex justify-center items-center fixed z-10 top-3">
-      <nav className=" flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop:blur">
+      <nav className=" flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         <a href=""className="nav-item">home</a>
         <a href=""className="nav-item">Projects</a>
         <a href=""className="nav-item">about</a>
