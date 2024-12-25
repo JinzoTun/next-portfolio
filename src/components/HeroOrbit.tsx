c9db34e9aab964784bf29bb8cd438b6821e17bef
Koussay Hajri <hajrikoussay03@gmail.com>
2024-12-25 01:29:59 +0100

    Enhance HeroOrbit component with spinning and orbiting animations; update HeroSection to utilize new props for dynamic effects

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,27 +1,47 @@
 import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
 
 export const HeroOrbit = ({ 
     children,
     size,
-    rotation 
-}: PropsWithChildren<{ size: number; rotation: number }> ) => {
+    rotation,
+    spinDuration,
+    orbitDuration,
+    isSpinning = false,
+    isOrbiting = false,
+}: PropsWithChildren<
+{ 
+  size: number; 
+  rotation: number;
+  isSpinning?: boolean;
+  isOrbiting?: boolean;
+  spinDuration?: number; 
+  orbitDuration?: number;
+}> ) => {
     return (
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2  ">
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-20 ">
+      <div className={twMerge(isOrbiting === true && "animate-spin " )}
+      style={{animationDuration: `${orbitDuration}s`}}>
       <div 
         className="flex items-start justify-start"
         style={{
         transform: `rotate(${rotation}deg)`,
         height: `${size}px`,
         width : `${size}px`,
-      }}
-    >
-        <div className="inline-flex" style={{
+      }}>
+        <div className={twMerge(isSpinning === true && "animate-spin ")} 
+        style={{
+            animationDuration: `${spinDuration}s`
+          }}>
+        <div className="inline-flex" 
+        style={{
             transform: `rotate(${rotation * -1}deg)`,
-        }} 
-        >
+        }}>
             {children}
         </div>
+      </div>
+      </div>
       </div>
     </div>
     )
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -10,7 +10,7 @@ import SparkleIcon from "@/assets/icons/sparkle.svg";
 export const HeroSection = () => {
   return (
   <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
-    <div className=" absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)] -z-10">
+    <div className=" absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)] -z-10 ">
     <div className="absolute inset-0 z-[-1] opacity-5 " 
     style={{
         backgroundImage: `url(${grainImage.src})`,
@@ -19,35 +19,41 @@ export const HeroSection = () => {
     <div className="size-[820px] hero-ring"></div>
     <div className="size-[1020px] hero-ring"></div>
     <div className="size-[1220px] hero-ring"></div>
-    <HeroOrbit  size={800} rotation={-72}>
-      <StarIcon className="size-28 text-emerald-300"/>
+    <HeroOrbit size={430} rotation={-14} isOrbiting orbitDuration={25} isSpinning spinDuration={3}>
+      <SparkleIcon className="size-8 text-emerald-300/20" />
     </HeroOrbit>
-    <HeroOrbit  size={550} rotation={20}>
-      <StarIcon className="size-12 text-emerald-300"/>
+    <HeroOrbit size={450} rotation={79} isOrbiting orbitDuration={28} isSpinning spinDuration={3}>
+      <SparkleIcon className="size-7 text-emerald-300/20" />
     </HeroOrbit>
-    <HeroOrbit  size={590} rotation={98}>
-      <StarIcon className="size-8 text-emerald-300"/>
+    <HeroOrbit size={520} rotation={-41} isOrbiting orbitDuration={31} isSpinning spinDuration={3}>
+      <SparkleIcon className="size-6 rounded-full text-emerald-300/20" />
     </HeroOrbit>
-    <HeroOrbit  size={430} rotation={-14}>
-      <SparkleIcon className="size-8 text-emerald-300/20"/>
+    <HeroOrbit size={540} rotation={178} isOrbiting orbitDuration={34} isSpinning spinDuration={3}>
+      <SparkleIcon className="size-10 text-emerald-300/20" />
     </HeroOrbit>
-    <HeroOrbit  size={440} rotation={79}>
-      <SparkleIcon className="size-5 text-emerald-300/20"/>
+    <HeroOrbit size={570} rotation={20} isOrbiting orbitDuration={37} isSpinning spinDuration={6}>
+      <StarIcon className="size-16 text-emerald-300" />
     </HeroOrbit>
-    <HeroOrbit  size={530} rotation={178}>
-      <SparkleIcon className="size-10 text-emerald-300/20"/>
+    <HeroOrbit size={600} rotation={98} isOrbiting orbitDuration={40} isSpinning spinDuration={6}>
+      <StarIcon className="size-12 text-emerald-300" />
     </HeroOrbit>
-    <HeroOrbit  size={710} rotation={144}>
-      <SparkleIcon className="size-14 text-emerald-300/20"/>
-      </HeroOrbit>
-    <HeroOrbit  size={720} rotation={85}>
-      <div className="size-3 rounded-full text-emerald-300/20"/>
+    <HeroOrbit size={660} rotation={-5} isOrbiting orbitDuration={43} isSpinning spinDuration={6}>
+      <StarIcon className="size-6 rounded-full text-emerald-300/20" />
     </HeroOrbit>
-    <HeroOrbit  size={520} rotation={-41}>
-      <div className="size-2 rounded-full text-emerald-300/20"/>
+    <HeroOrbit size={720} rotation={144} isOrbiting orbitDuration={46} isSpinning spinDuration={3}>
+      <SparkleIcon className="size-18 text-emerald-300/20" />
     </HeroOrbit>
-    <HeroOrbit  size={650} rotation={-5}>
-      <div className="size-2 rounded-full text-emerald-300/20"/>
+    <HeroOrbit size={740} rotation={85} isOrbiting orbitDuration={49} isSpinning spinDuration={6}>
+      <StarIcon className="size-6 rounded-full text-emerald-300/20" />
+    </HeroOrbit>
+    <HeroOrbit size={820} rotation={-72} isOrbiting orbitDuration={52} isSpinning spinDuration={6}>
+      <StarIcon className="size-22 text-emerald-300" />
+    </HeroOrbit>
+    <HeroOrbit size={950} rotation={45} isOrbiting orbitDuration={55} isSpinning spinDuration={6}>
+      <StarIcon className="size-28 text-emerald-300" />
+    </HeroOrbit>
+    <HeroOrbit size={1100} rotation={-90} isOrbiting orbitDuration={58} isSpinning spinDuration={6}>
+      <StarIcon className="size-36 text-emerald-300" />
     </HeroOrbit>
     </div>
     <div className="container"> 
@@ -75,7 +81,7 @@ export const HeroSection = () => {
         </button>
         <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900  h-12 px-6 rounded-xl">
           <span>👋</span>
-          <span className="font-semibold">Let&apos;s Connect</span>
+          <span className="font-semibold">Let's Connect</span>
         </button>
       </div>
     </div>
